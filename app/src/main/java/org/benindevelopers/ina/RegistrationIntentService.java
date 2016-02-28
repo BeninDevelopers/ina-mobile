74599a453e7026c736d01acc3bf1c6ed88f640c6
maxoreli <maxoreli1@gmail.com>
2016-02-28 05:49:33 +0100

    Integrating GCM Notif

---
+++ app/src/main/java/org/benindevelopers/ina/RegistrationIntentService.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.benindevelopers.ina;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+
+
+public class RegistrationIntentService extends IntentService {
+
+    private static final String TAG = "RegIntentService";
+    private static final String[] TOPICS = {"global"};
+    private static String register_id="";
+
+    public RegistrationIntentService()
+    {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        try {
+            // [START register_for_gcm]
+            // Initially this call goes out to the network to retrieve the token, subsequent calls
+            // are local.
+            // R.string.gcm_defaultSenderId (the Sender ID) is typically derived from google-services.json.
+            // See https://developers.google.com/cloud-messaging/android/start for details on this file.
+            // [START get_token]
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(getString(R.string.gcm_reg_id),GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            // [END get_token]
+            Toast.makeText(this,token,Toast.LENGTH_LONG).show();
+            Log.e(TAG, "GCM Registration Token: " + token);
+
+            // TODO: Implement this method to send any registration to your app's servers.
+            sendRegistrationToServer(token);
+//            register_id=token;
+
+            // Subscribe to topic channels
+
+            // You should store a boolean that indicates whether the generated token has been
+            // sent to your server. If the boolean is false, send the token to your server,
+            // otherwise your server should have already received the token.
+            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, true).apply();
+            // [END register_for_gcm]
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to complete token refresh", e);
+            // If an exception happens while fetching the new token or updating our registration data
+            // on a third-party server, this ensures that we'll attempt the update at a later time.
+            sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
+        }
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        //Intent registrationComplete = new Intent(QuickstartPreferences.REGISTRATION_COMPLETE);
+        //LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(String gcmId) {
+
+    }
+
+    /**
+     * Persist registration to third-party servers.
+     *
+     * Modify this method to associate the user's GCM registration token with any server-side account
+     * maintained by your application.
+     *
+     *
+     */
+    public static String getRegistrationId() {
+        // Add custom implementation, as needed.
+
+        return register_id;
+    }
+
+    /**
+     * Subscribe to any GCM topics of interest, as defined by the TOPICS constant.
+     *
+     * @param token GCM token
+     * @throws IOException if unable to reach the GCM PubSub service
+     */
+
+
+}
