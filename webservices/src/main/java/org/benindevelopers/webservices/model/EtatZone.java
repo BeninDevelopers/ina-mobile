aac1bf05e9e6890086155e17e4580cb88e60aec9
GNAVO <gphares7@gmail.com>
2016-02-28 04:18:05 +0100

    Added getters and setters for the model classes.

--- webservices/src/main/java/org/benindevelopers/webservices/model/EtatZone.java
+++ webservices/src/main/java/org/benindevelopers/webservices/model/EtatZone.java
@@ -5,11 +5,58 @@ package org.benindevelopers.webservices.model;
  */
 public class EtatZone {
 
-    long id;
-    float lat;
-    float lon;
-    boolean etat;
-    String zone;
-    String description;
+    private long id;
+    private float lat;
+    private float lon;
+    private boolean etat;
+    private String zone;
+    private String description;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public boolean isEtat() {
+        return etat;
+    }
+
+    public void setEtat(boolean etat) {
+        this.etat = etat;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
